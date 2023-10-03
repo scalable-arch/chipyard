758c95b805caa20b18b69e94abbfce633de3ede4
Jerry Zhao <jerryz123@berkeley.edu>
2023-10-03 10:01:35 -0700

    Bump vectors

--- generators/chipyard/src/main/scala/config/VectorConfigs.scala
+++ generators/chipyard/src/main/scala/config/VectorConfigs.scala
@@ -48,3 +48,14 @@ class V256D128Config extends Config(
   new freechips.rocketchip.subsystem.WithRocketDebugROB ++
   new freechips.rocketchip.subsystem.WithNBigCores(1) ++
   new chipyard.config.AbstractConfig)
+
+class V512D128Config extends Config(
+  new chipyard.harness.WithCospike ++
+  new chipyard.config.WithTraceIO ++
+  new vector.rocket.WithRocketVectorUnit(512, 128) ++
+  new chipyard.config.WithSystemBusWidth(128) ++
+  new freechips.rocketchip.subsystem.WithRocketCease(false) ++
+  new freechips.rocketchip.subsystem.WithRocketDebugROB ++
+  new freechips.rocketchip.subsystem.WithNBigCores(1) ++
+  new chipyard.config.AbstractConfig)
+
