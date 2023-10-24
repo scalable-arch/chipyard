af991728f017cf01054ee08f55be6ad12ce33814
Vighnesh Iyer <vighnesh.iyer@berkeley.edu>
2023-10-24 01:22:40 +0000

    force based pc injection seems ok

--- variables.mk
+++ variables.mk
@@ -21,6 +21,7 @@ HELP_PROJECT_VARIABLES = \
 "   CONFIG_PACKAGE         = the scala package to find the CONFIG class [$(CONFIG_PACKAGE)]" \
 "   GENERATOR_PACKAGE      = the scala package to find the Generator class in [$(GENERATOR_PACKAGE)]" \
 "   TB                     = testbench wrapper over the TestHarness needed to simulate in a verilog simulator [$(TB)]" \
+"   STATE_INJECT           = set to 1 to use a TestDriver.v that is capable of injecting state into the DUT \
 "   TOP                    = top level module of the project (normally the module instantiated by the harness) [$(TOP)]"
 
 HELP_SIMULATION_VARIABLES = \
@@ -275,7 +276,11 @@ PERMISSIVE_ON=+permissive
 PERMISSIVE_OFF=+permissive-off
 BINARY ?=
 BINARIES ?=
+ifdef STATE_INJECT
+override SIM_FLAGS += +dramsim +dramsim_ini_dir=$(TESTCHIP_DIR)/src/main/resources/dramsim2_ini +max-cycles=$(timeout_cycles) +no_hart0_msip
+else
 override SIM_FLAGS += +dramsim +dramsim_ini_dir=$(TESTCHIP_DIR)/src/main/resources/dramsim2_ini +max-cycles=$(timeout_cycles)
+endif
 VERBOSE_FLAGS ?= +verbose
 # get_out_name is a function, 1st argument is the binary
 get_out_name = $(subst $() $(),_,$(notdir $(basename $(1))))
